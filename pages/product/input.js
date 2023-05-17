5eda7e91431c7f63195de5c9807c80098b7e8787
jqwirq <dummy.wiji@gmail.com>
2023-05-17 14:05:01 +0700

    update

--- pages/product/input.js
+++ pages/product/input.js
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Link from "next/link";
 
 export default function Page() {
-  const [shithead, setShithead] = useState([]);
+  const [productNo, setProductNo] = useState("");
+  const [isInput, setIsInput] = useState(false);
+  const [materialsNo, setMaterialsNo] = useState([]);
+
+  const materialNo = useRef();
+
+  const handleClickSubmit = () => {
+    if (productNo === "") {
+      console.log("it's empty");
+    } else {
+      console.log("product not empty");
+    }
+  };
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -14,17 +26,61 @@ export default function Page() {
       <h1 className="text-3xl text-center pt-2">Input Product</h1>
 
       <div className="grow flex flex-col gap-2 pb-10 px-10 md:px-[11%] lg:px-[14%] pt-6">
+        {/* INPUT PRODUCT NO */}
         <div className="text-lg flex gap-4 items-center px-4">
-          <div>Product No :</div>
-          <input className="grow px-3 py-1 tracking-widest" type="text" />
+          <div className="">Product No</div>
+          <div>:</div>
+          <input
+            className="grow px-3 py-1 tracking-widest"
+            type="text"
+            onChange={(e) => {
+              const value = e.target.value;
+              setProductNo(() => value);
+            }}
+          />
         </div>
 
-        <button className="mx-10 bg-slate-300 text-lg">
-          Fucking add a material you dickhead
-        </button>
-        <div className="grow">Fucking material list you motherfucker</div>
+        <div className="text-base flex gap-4 items-center px-4">
+          <div className="">Material No</div>
+          <div>:</div>
+          <input
+            ref={materialNo}
+            className="grow px-3 py-1 tracking-widest"
+            type="text"
+          />
 
-        <button className="py-1 mx-10 bg-slate-300 text-lg">Submit</button>
+          <button
+            className="bg-slate-300 p-1 text-xs"
+            onClick={() => {
+              const v = materialNo.current.value;
+
+              if (v === "") {
+                console.log("fuck you");
+              } else {
+                console.log(v);
+                setMaterialsNo((state) => [...state, v]);
+                materialNo.current.value = "";
+              }
+            }}
+          >
+            Add Material
+          </button>
+        </div>
+        <div className="grow">
+          {materialsNo.length === 0
+            ? "Empty"
+            : materialsNo.map((v, i) => {
+                return <div key={i}>{v}</div>;
+              })}
+        </div>
+
+        <button
+          className="py-1 mx-10 bg-slate-300 text-lg"
+          onClick={handleClickSubmit}
+          disabled={productNo === ""}
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
