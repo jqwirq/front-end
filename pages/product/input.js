cbe7c20cb8b86048a23ac1f41d9959a370a586b4
jqwirq <dummy.wiji@gmail.com>
2023-05-19 23:22:53 +0700

    update input product

--- pages/product/input.js
+++ pages/product/input.js
@@ -6,12 +6,19 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export default function Page() {
   const [productNo, setProductNo] = useState("");
   const [materialsNo, setMaterialsNo] = useState([]);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setisError] = useState(false);
 
   const materialNo = useRef();
 
   const handleClickSubmit = async () => {
     if (productNo === "") {
-      console.log("it's empty");
+      console.log("product is empty");
+      return;
+    }
+
+    if (materialsNo.length === 0) {
+      console.log("material is empty");
       return;
     }
 
@@ -45,74 +52,84 @@ export default function Page() {
   }, [materialsNo]);
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="bg-slate-200 basis-8 px-4 flex justify-between items-center">
-        <Link className="" href="/product">
-          back
-        </Link>
-      </div>
-      <h1 className="text-3xl text-center pt-2">Input Product</h1>
-
-      <div className="grow flex flex-col gap-2 pb-10 px-10 md:px-[11%] lg:px-[14%] pt-6">
-        <div className="text-lg flex gap-4 items-center px-4">
-          <div className="">Product No</div>
-          <div>:</div>
-          <input
-            className="grow px-3 py-1 tracking-widest appearance-none"
-            type="number"
-            onChange={(e) => {
-              const value = e.target.value;
-              setProductNo(() => value);
-            }}
-          />
+    <>
+      <div className="min-h-screen flex flex-col">
+        <div className="bg-slate-200 basis-8 px-4 flex justify-between items-center">
+          <Link className="" href="/product">
+            back
+          </Link>
         </div>
+        <h1 className="text-3xl text-center pt-2">Input Product</h1>
 
-        <div className="text-base flex gap-4 items-center px-4">
-          <div className="">Material No</div>
-          <div>:</div>
-          <input
-            ref={materialNo}
-            className="grow px-3 py-1 tracking-widest"
-            type="number"
-          />
+        <div className="grow flex flex-col gap-2 pb-10 px-10 md:px-[11%] lg:px-[14%] pt-6">
+          <div className="text-lg flex gap-4 items-center px-4">
+            <div className="">Product No</div>
+            <div>:</div>
+            <input
+              className="grow px-3 py-1 tracking-widest appearance-none"
+              type="number"
+              onChange={(e) => {
+                const value = e.target.value;
+                setProductNo(() => value);
+              }}
+            />
+          </div>
+
+          <div className="text-base flex gap-4 items-center px-4">
+            <div className="">Material No</div>
+            <div>:</div>
+            <input
+              ref={materialNo}
+              className="grow px-3 py-1 tracking-widest"
+              type="number"
+            />
+
+            <button
+              className="bg-slate-300 p-1 text-xs"
+              onClick={() => {
+                const v = materialNo.current.value;
+
+                if (v === "") {
+                  console.log("empty");
+                } else if (v.length < 8 || v.length > 12) {
+                  console.log("length");
+                } else {
+                  console.log(v);
+                  setMaterialsNo((state) => [...state, v]);
+                  materialNo.current.value = "";
+                }
+              }}
+            >
+              Add Material
+            </button>
+          </div>
+          <div className="grow text-center">
+            {materialsNo.length === 0 ? (
+              <div>empty</div>
+            ) : (
+              materialsNo.map((v, i) => {
+                return <div key={i}>{v}</div>;
+              })
+            )}
+          </div>
 
           <button
-            className="bg-slate-300 p-1 text-xs"
-            onClick={() => {
-              const v = materialNo.current.value;
-
-              if (v === "") {
-                console.log("fuck you");
-              } else if (v.length < 8 || v.length > 12) {
-                console.log("you dick head");
-              } else {
-                console.log(v);
-                setMaterialsNo((state) => [...state, v]);
-                materialNo.current.value = "";
-              }
-            }}
+            className="py-1 mx-10 bg-slate-300 text-lg"
+            onClick={handleClickSubmit}
+            disabled={productNo === ""}
           >
-            Add Material
+            Submit
           </button>
         </div>
-        <div className="grow text-center">
-          {materialsNo.length === 0 ? (
-            <div>empty</div>
-          ) : (
-            materialsNo.map((v, i) => {
-              return <div key={i}>{v}</div>;
-            })
-          )}
-        </div>
-
-        <button
-          className="py-1 mx-10 bg-slate-300 text-lg"
-          onClick={handleClickSubmit}
-          disabled={productNo === ""}
-        >
-          Submit
-        </button>
       </div>
-    </div>
+
+      {/* ALERT */}
+      <div className="bg-slate-900/50 fixed inset-0 flex justify-center items-center">
+        <div className="bg-slate-100 p-4 flex flex-col gap-4">
+          <div>alert message</div>
+          <button className="bg-slate-300">close</button>
+        </div>
+      </div>
+    </>
   );
 }
