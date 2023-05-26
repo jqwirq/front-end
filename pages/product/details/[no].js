4caece6217f9609ec68a91687e9d0376b3449600
jqwirq <dummy.wiji@gmail.com>
2023-05-26 20:09:16 +0700

    update

--- pages/product/details/[no].js
+++ pages/product/details/[no].js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getServerSideProps(context) {
@@ -16,10 +18,86 @@ export async function getServerSideProps(context) {
 function YourPage({ responseJson }) {
   console.log(responseJson);
   return (
-    <div>
-      <h1>hallo</h1>
+    <div className="min-h-screen max-h-screen flex flex-col">
+      <div className="bg-slate-900 text-slate-200 basis-12 px-6 flex justify-between items-center">
+        <Link
+          className="text-xl hover:text-slate-300 active:text-slate-200"
+          href="/product/list"
+        >
+          back
+        </Link>
+      </div>
+
+      <h1 className="text-5xl text-center pt-4">Product Details</h1>
+
+      <div className="grow flex flex-col gap-8 py-10 px-10 md:px-[8%] lg:px-[10%]">
+        {!responseJson && (
+          <div className="text-center text-5xl pb-20 grow flex justify-center items-center text-slate-400">
+            Empty
+          </div>
+        )}
+        {responseJson && (
+          <>
+            <div className="text-3xl flex justify-between items-center">
+              <div className="">Product No</div>
+              <div className="relative basis-2/3">
+                <input
+                  className={`w-full px-3 py-1 tracking-widest appearance-none focus:outline-none focus:ring-2`}
+                  // ref={productNoRef}
+                  defaultValue={responseJson.no}
+                  type="text"
+                  // onChange={handleInputProductNoChange}
+                />
+                {/* {!isProductNoValid && (
+                  <div className="absolute bottom-[-18px] text-red-500 text-xs">
+                    Input must number!
+                  </div>
+                )} */}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
+
+// {products.length !== 0 && (
+//           <table className="table-auto text-lg border-collapse">
+//             <thead className="border-b-2 border-slate-950">
+//               <tr>
+//                 <th className="p-2">Product No.</th>
+//                 <th className="p-2">Created At</th>
+//                 <th className="p-2">Updated At</th>
+//                 <th className="p-2">Action</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {products.map((v, i) => {
+//                 let createdAt = new Date(v.createdAt);
+//                 let updatedAt = new Date(v.updatedAt);
+//                 return (
+//                   <tr
+//                     className="text-center border-b border-slate-950"
+//                     key={v.no}
+//                   >
+//                     <td className="p-2">{v.no}</td>
+//                     <td className="p-2">{createdAt.toLocaleString("en-UK")}</td>
+//                     <td className="p-2">{updatedAt.toLocaleString("en-UK")}</td>
+//                     <td className="p-2">
+//                       {/* <Modal product={v} /> */}
+//                       <Link
+//                         className="text-slate-600 hover:text-slate-500 active:text-slate-600"
+//                         href={`/product/details/${v.no}`}
+//                       >
+//                         details
+//                       </Link>
+//                     </td>
+//                   </tr>
+//                 );
+//               })}
+//             </tbody>
+//           </table>
+//         )}
 
 export default YourPage;
--- pages/product/list.js
+++ pages/product/list.js
@@ -21,7 +21,10 @@ export default function Page() {
   return (
     <div className="min-h-screen max-h-screen flex flex-col">
       <div className="bg-slate-900 text-slate-200 basis-12 px-6 flex justify-between items-center">
-        <Link className="text-xl hover:text-slate-300 active:text-slate-200" href="/product">
+        <Link
+          className="text-xl hover:text-slate-300 active:text-slate-200"
+          href="/product"
+        >
           back
         </Link>
       </div>
@@ -29,7 +32,9 @@ export default function Page() {
       <h1 className="text-5xl text-center pt-4">Product List</h1>
 
       <div className="grow overflow-auto flex flex-col pt-6 md:px-[8%] lg:px-[10%]">
-        {products.length === 0 && <div className="text-center grow text-slate-400">empty</div>}
+        {products.length === 0 && (
+          <div className="text-center grow text-slate-400">empty</div>
+        )}
         {products.length !== 0 && (
           <table className="table-auto text-lg border-collapse">
             <thead className="border-b-2 border-slate-950">
@@ -53,7 +58,13 @@ export default function Page() {
                     <td className="p-2">{createdAt.toLocaleString("en-UK")}</td>
                     <td className="p-2">{updatedAt.toLocaleString("en-UK")}</td>
                     <td className="p-2">
-                      <Modal product={v} />
+                      {/* <Modal product={v} /> */}
+                      <Link
+                        className="text-slate-600 hover:text-slate-500 active:text-slate-600"
+                        href={`/product/details/${v.no}`}
+                      >
+                        details
+                      </Link>
                     </td>
                   </tr>
                 );
@@ -65,113 +76,3 @@ export default function Page() {
     </div>
   );
 }
-
-function Modal({ product }) {
-  const [isShow, setisShow] = useState(false);
-  console.log(product.materials);
-
-  return (
-    <>
-      <button
-        className="text-slate-600 hover:text-slate-500 active:text-slate-600"
-        onClick={() => setisShow(true)}
-      >
-        details
-      </button>
-
-      {isShow && (
-        <div className="bg-slate-900/50 fixed inset-0 flex justify-center items-center">
-          <div className={`p-4 max-w-[80%] flex flex-col gap-4 bg-slate-100`}>
-            <div className="text-base text-left">
-              <div className="mb-2">Product no. : {product.no}</div>
-              <div className="">Materials:</div>
-              <ul className="list-disc pl-6">
-                {product &&
-                  product.materials.map((m) => <li key={m.no}>{m.no}</li>)}
-              </ul>
-            </div>
-
-            <button
-              onClick={() => setisShow(false)}
-              className="bg-slate-300 hover:bg-slate-400 active:bg-slate-300 py-1 px-2"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      )}
-    </>
-  );
-}
-
-// THIS IS WORK
-// return (
-//   <div className="min-h-screen max-h-screen flex flex-col">
-//     <div className="bg-slate-200 basis-8 shrink-0 px-4 flex justify-between items-center">
-//       <Link className="" href="/product">
-//         back
-//       </Link>
-//     </div>
-
-//     <h1 className="text-3xl text-center pt-2">Product List</h1>
-
-//     <div className="grow overflow-auto flex flex-col flex-wrap gap-8 pb-10 pt-6 px-10 md:px-[11%] lg:px-[14%]">
-//       {products.length === 0 && "Empty"}
-//       {products.length !== 0 &&
-//         products.map((v, i) => {
-//           return (
-//             <div
-//               className="flex items-center gap-6 text-center bg-slate-200"
-//               key={v.no}
-//             >
-//               <div className="grow">{v.no}</div>
-//             </div>
-//           );
-//         })}
-//     </div>
-//   </div>
-// );
-
-// return (
-//   <div
-//     className="flex items-center gap-6 text-center bg-slate-200"
-//     key={v.no}
-//   >
-//     <div className="grow">{v.no}</div>
-//     {/* <div className="grow">
-//       {v.materials.map((x, j) => {
-//         return <div key={x.no}>{x.no}</div>;
-//       })}
-//     </div> */}
-//   </div>
-
-// return (
-
-// )
-
-// return (
-//   <div className="min-h-screen max-h-screen flex flex-col">
-//     <div className="bg-slate-200 flex-shrink-0 px-4 flex justify-between items-center">
-//       <Link className="" href="/product">
-//         back
-//       </Link>
-//     </div>
-
-//     <h1 className="text-3xl text-center pt-2 flex-shrink-0">Product List</h1>
-
-//     <div className="flex-grow overflow-auto flex flex-col flex-wrap gap-8 pb-10 pt-6 px-10 md:px-[11%] lg:px-[14%]">
-//       {products.length === 0 && "Empty"}
-//       {products.length !== 0 &&
-//         products.map((v, i) => {
-//           return (
-//             <div
-//               className="flex items-center gap-6 text-center bg-slate-200"
-//               key={v.no}
-//             >
-//               <div className="grow">{v.no}</div>
-//             </div>
-//           );
-//         })}
-//     </div>
-//   </div>
-// );
