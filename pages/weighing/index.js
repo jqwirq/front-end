ba3db640a28d660e208359707e63356d1394a0ff
jqwirq <dummy.wiji@gmail.com>
2023-05-31 16:35:20 +0700

    update

--- pages/weighing/index.js
+++ pages/weighing/index.js
@@ -40,34 +40,34 @@ export default function Page() {
     <>
       <div className="bg-slate-100 min-h-screen">
         <div className="min-h-screen max-h-screen flex flex-col">
-          <div className="bg-slate-900 text-slate-200 basis-8 shrink-0 px-4 flex justify-between items-center">
-            <Link className="" href="/">
+          <div className="bg-slate-900 text-slate-200 basis-12 px-6 flex justify-between items-center">
+            <Link
+              className="text-xl hover:text-slate-300 active:text-slate-200"
+              href="/"
+            >
               back
             </Link>
 
-            <h1 className="tracking-widest font-semibold">Weighing Process</h1>
+            <h1 className="tracking-widest font-semibold text-xl">
+              Weighing Process
+            </h1>
           </div>
 
-          <div className="grow grid grid-cols-10 grid-rows-6 gap-4 px-2 md:px-[1%] lg:px-[3%] py-2 text-2xl">
-            <div className="col-span-6 row-span-4 flex flex-col gap-4 px-8 text-sm justify-center">
-              <FormWeighing />
-            </div>
+          <div
+            className="grow grid grid-cols-10 grid-rows-6 gap-4 px-2 md:px-[1%] lg:px-[3%] py-2 text-2xl"
+            style={{ gridTemplateRows: "1fr 1fr 1fr 1fr 1fr 1.5fr" }}
+          >
+            <FormWeighing />
 
-            <div className="col-start-1 col-end-6 row-start-5 row-end-7 flex flex-col justify-center items-center gap-4 px-8 text-sm">
-              <THE_BUTTONS handleClick={handleClick} />
-            </div>
+            <ScaleSelectButton handleClick={handleClick} />
 
             <div className="col-start-7 col-end-13 row-start-1 row-end-3 flex flex-col justify-center items-center gap-4">
               <THE_TIMER />
             </div>
 
-            <div className="col-start-7 col-end-13 row-start-3 row-end-5 flex justify-center items-center gap-4 text-sm">
-              <THE_BUTTONS_2 />
-            </div>
+            <StartButton />
 
-            <div className="col-start-6 col-end-13 row-start-5 row-end-7 flex flex-col justify-start items-center gap-4">
-              <Weight eventSource={eventSource} data={data} />
-            </div>
+            <Weight eventSource={eventSource} data={data} />
           </div>
         </div>
       </div>
@@ -77,15 +77,16 @@ export default function Page() {
 
 function Weight({ eventSource, data }) {
   return (
-    <>
+    <div className="col-start-5 col-end-13 row-start-5 row-end-7 flex flex-col justify-start items-center px-2 gap-4">
       <div className="text-9xl bg-yellow-200 font-bold w-full p-4 text-center">
         {data ? data : "0.00KG"}
       </div>
-      <div className="flex justify-center text-2xl text-center gap-2 w-full">
+
+      <div className="flex justify-center text-3xl text-center gap-4 w-full">
         <div className="bg-red-400 basis-1/2 py-2 px-4">- 0 KG.</div>
         <div className="bg-red-400 basis-1/2 py-2 px-4">+ 0 KG.</div>
       </div>
-    </>
+    </div>
   );
 }
 
@@ -98,6 +99,7 @@ function THE_TIMER() {
           00:00:00
         </div>
       </div>
+
       <div className="flex flex-col items-center">
         <div className="text-xs">Duration per Product</div>
         <div className="bg-black text-white text-center w-[120px] py-2">
@@ -108,93 +110,96 @@ function THE_TIMER() {
   );
 }
 
-function THE_BUTTONS_2() {
+function StartButton() {
   return (
-    <>
-      <button className="bg-green-600 hover:bg-green-500 text-white w-[120px] py-2">
+    <div className="px-2 pb-8 col-start-7 col-end-13 row-start-3 row-end-5 flex justify-center items-center gap-4 text-xl">
+      <button className="bg-green-600 hover:bg-green-500 basis-1/2 text-white py-4">
         Start material
       </button>
-      <button className="bg-green-600 hover:bg-green-500 text-white w-[120px] py-2">
+
+      <button className="bg-green-600 hover:bg-green-500 basis-1/2 text-white py-4">
         Start product
       </button>
-    </>
+    </div>
   );
 }
 
-function THE_BUTTONS({ handleClick }) {
+function ScaleSelectButton({ handleClick }) {
   return (
-    <>
-      <div className="flex gap-4 basis-10 justify center">
+    <div className="col-start-1 col-end-5 row-start-5 pb-8 row-end-7 flex flex-col justify-center items-center gap-4 text-4xl">
+      <div className="flex w-full gap-4 justify-center items-center">
         <button
           onClick={handleClick}
-          className="bg-slate-400 hover:bg-slate-300 w-[100px]"
+          className="bg-slate-400 py-2 hover:bg-slate-300 basis-1/2"
         >
           2 ton
         </button>
-        <button className="bg-slate-400 hover:bg-slate-300 w-[100px]">
+
+        <button className="bg-slate-400 py-2 hover:bg-slate-300 basis-1/2">
           350 Kg
         </button>
       </div>
-      <div className="flex gap-4 basis-10 justify center">
-        <button className="bg-slate-400 hover:bg-slate-300 w-[100px]">
+      <div className="flex w-full gap-4 justify-center items-center">
+        <button className="bg-slate-400 py-2 hover:bg-slate-300 basis-1/2">
           2 Kg
         </button>
-        <button className="bg-slate-400 hover:bg-slate-300 w-[100px]">
+
+        <button className="bg-slate-400 py-2 hover:bg-slate-300 basis-1/2">
           ...
         </button>
       </div>
-    </>
+    </div>
   );
 }
 
 function FormWeighing() {
   return (
-    <>
+    <div className="col-span-6 row-span-4 flex flex-col gap-4 pl-4 text-md justify-center">
       <div className="flex justify-between items-center">
         <div>SAP Order No.</div>
-        <input className="w-[50%] bg-yellow-200 p-1 text-[14px]" type="text" />
+        <input className="w-[50%] bg-yellow-200 p-1" type="text" />
       </div>
 
       <div className="flex justify-between items-center">
         <div>Batch No.</div>
-        <input className="w-[50%] bg-yellow-200 p-1 text-[14px]" type="text" />
+        <input className="w-[50%] bg-yellow-200 p-1" type="text" />
       </div>
 
       <div className="flex justify-between items-center">
         <div>Product No.</div>
-        <select className="w-[50%] bg-yellow-200 p-1 text-[14px]">
+        <select className="w-[50%] bg-yellow-200 p-1">
           <option value=""></option>
         </select>
       </div>
 
       <div className="flex justify-between items-center">
         <div>Material No.</div>
-        <select className="w-[50%] bg-yellow-200 p-1 text-[14px]">
-          <option value=""></option>
+        <select className="w-[50%] bg-yellow-200 p-1">
+          <option value="test">Test</option>
         </select>
       </div>
 
       <div className="flex justify-between items-center">
         <div>Packaging</div>
-        <select className="w-[50%] bg-yellow-200 p-1 text-[14px]">
+        <select className="w-[50%] bg-yellow-200 p-1">
           <option value=""></option>
         </select>
       </div>
 
       <div className="flex justify-between items-center">
         <div>Batch No.</div>
-        <input className="w-[50%] bg-yellow-200 p-1 text-[14px]" type="text" />
+        <input className="w-[50%] bg-yellow-200 p-1" type="text" />
       </div>
 
       <div className="flex justify-between items-center">
         <div>Target Qty. (Kg.)</div>
-        <input className="w-[50%] bg-yellow-200 p-1 text-[14px]" type="text" />
+        <input className="w-[50%] bg-yellow-200 p-1" type="text" />
       </div>
 
       <div className="flex justify-between items-center">
         <div>Tolerance (%)</div>
-        <input className="w-[50%] bg-yellow-200 p-1 text-[14px]" type="text" />
+        <input className="w-[50%] bg-yellow-200 p-1" type="text" />
       </div>
-    </>
+    </div>
   );
 }
