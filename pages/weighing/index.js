da1a60cb5aa24adb0950383465e9618eaaf72aa9
jqwirq <dummy.wiji@gmail.com>
2023-06-23 15:28:00 +0700

    fix scale

--- pages/weighing/index.js
+++ pages/weighing/index.js
@@ -1379,7 +1379,7 @@ function ScaleSelectButton() {
           }`}
           disabled={!isMaterialProcess}
         >
-          350 T
+          300 TS
         </button>
       </div>
       <div className='flex w-full gap-4 justify-center items-center'>
@@ -1392,7 +1392,7 @@ function ScaleSelectButton() {
           }`}
           disabled={!isMaterialProcess}
         >
-          350 J
+          300 JIC
         </button>
 
         <button
